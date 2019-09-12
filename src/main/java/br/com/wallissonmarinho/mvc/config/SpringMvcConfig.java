f88c0e1fdf2e5e9a2751ddd81c9c92f9d01c0304
wallissonmarinho <wallissonmarinho@hotmail.com>
2019-09-12 13:18:59 -0300

    messages.properties adicionado

--- src/main/java/br/com/wallissonmarinho/mvc/config/SpringMvcConfig.java
+++ src/main/java/br/com/wallissonmarinho/mvc/config/SpringMvcConfig.java
@@ -1,7 +1,9 @@
 package br.com.wallissonmarinho.mvc.config;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -24,6 +26,13 @@ public class SpringMvcConfig extends WebMvcConfigurerAdapter{
 	@Override
 	public void addFormatters(FormatterRegistry registry) {
 		registry.addConverter(new TipoSexoConverter());
+	}
+	
+	@Bean
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+		source.setBasename("messages");
+		return source;
 	}
 	
 	
